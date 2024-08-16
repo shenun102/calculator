dd08dbdca349e6f01e95ebf10503f1e63c0936a2
shenun102 <124623141+shenun102@users.noreply.github.com>
2024-08-16 15:03:16 +0100

    Add functionality to AC clear and dot buttons

--- calculator.js
+++ calculator.js
@@ -1,46 +1,78 @@
 const displayResult = document.querySelector(".result");
 const btnContainer = document.querySelector(".buttons-container");
-console.log(displayResult);
+// console.log(displayResult);
 
 btnContainer.addEventListener("click", handleClick);
 
+let operation = false;
+let calcResult = false;
+let operator;
+let numOne = 0;
+let numTwo = 0;
+let firstOrSec = 1;
+
 function handleClick(event) {
   let target = event.target;
-  // if the target element doesnt have a class name of "btn" return
+  // if the target element doesnt have a class name of "btn", return
   if (!target.className.includes("btn")) return;
-  console.log(event.target.textContent);
-  updateDisplay(target.textContent);
+  // console.log(target.textContent);
+
+  // checks if its clear display button AC
+  if (target.className.includes("btn-c")) {
+    handleClear();
+    return;
+  }
+
+  // checks for dot
+  if (target.className.includes("dot")) {
+    if (!displayResult.textContent.includes(".")) {
+      updateDisplay(".");
+    } else return;
+  }
+
+  if (target.className.includes("operate-button")) {
+    // Check if the button was an operator
+    operation = true;
+    operator = target.textContent;
+  }
 }
+
+// Handle clear function
+
+function handleClear() {
+  displayResult.textContent = "0";
+}
+
+// Update the display function
 
 function updateDisplay(text) {
   if (displayResult.textContent === "0") {
-    displayResult.textContent = "";
+    // if its dot
+    if (text === ".") {
+      displayResult.textContent += text;
+      return;
+    } else displayResult.textContent = "";
   }
+
   displayResult.textContent += text;
 }
 
 // 1. Basic Math Operations
 function add(num1, num2) {
-  return num1 + num2;
+  return parseFloat(num1) + parseFloat(num2);
 }
 
 function subtract(num1, num2) {
-  return num1 - num2;
+  return parseFloat(num1) - parseFloat(num2);
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return parseFloat(num1) * parseFloat(num2);
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  return parseFloat(num1) / parseFloat(num2);
 }
-
-console.log(add(1, 1));
-
-let operator = "";
-let firstNum = 1;
-let secondNum = 2;
 
 // 3. Operate Function
 
@@ -50,7 +82,5 @@ function operate(operator, firstNum, secondNum) {
   if (operator === "-") result = subtract(firstNum, secondNum);
   if (operator === "*") result = multiply(firstNum, secondNum);
   if (operator === "/") result = divide(firstNum, secondNum);
-  updateDisplay(result);
+  return result;
 }
-
-operate("+", 1, 1);
